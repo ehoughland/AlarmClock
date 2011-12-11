79c3f8aa147f0854fd5e2ee5153350327311db87
Eric Houghland <eric.houghland@gmail.com>
2011-12-11 14:51:37 -0800

    added code to gather some UI elements and save/read preferences

--- gen/edu/washington/ehoughl_krtyler1/R.java
+++ gen/edu/washington/ehoughl_krtyler1/R.java
@@ -10,39 +10,46 @@ package edu.washington.ehoughl_krtyler1;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int darkestgray=0x7f040003;
+        public static final int darkgray=0x7f040002;
+        public static final int lightgray=0x7f040000;
+        public static final int mediumgray=0x7f040001;
+    }
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int buttonselected=0x7f020000;
+        public static final int icon=0x7f020001;
     }
     public static final class id {
-        public static final int buttonCancel=0x7f050017;
-        public static final int buttonFriday=0x7f050010;
-        public static final int buttonMonday=0x7f05000c;
-        public static final int buttonSaturday=0x7f050011;
-        public static final int buttonSave=0x7f050016;
-        public static final int buttonSunday=0x7f05000b;
-        public static final int buttonThursday=0x7f05000f;
-        public static final int buttonTuesday=0x7f05000d;
-        public static final int buttonWednesday=0x7f05000e;
-        public static final int rel1=0x7f050003;
-        public static final int relativeLayout1=0x7f05000a;
-        public static final int seekBar1=0x7f050014;
-        public static final int spinner1=0x7f050012;
-        public static final int textView1=0x7f050013;
-        public static final int textView2=0x7f050015;
-        public static final int textViewAlarmTitle=0x7f050000;
-        public static final int textViewBedTime1=0x7f050004;
-        public static final int textViewBedTime1AMPM=0x7f050005;
-        public static final int textViewBedTime2=0x7f050006;
-        public static final int textViewBedTime2AMPM=0x7f050007;
-        public static final int textViewBedTime3=0x7f050008;
-        public static final int textViewBedTime3AMPM=0x7f050009;
-        public static final int textViewSuggestion=0x7f050002;
-        public static final int timePicker1=0x7f050001;
+        public static final int buttonCancel=0x7f060017;
+        public static final int buttonFriday=0x7f060010;
+        public static final int buttonMonday=0x7f06000c;
+        public static final int buttonSaturday=0x7f060011;
+        public static final int buttonSave=0x7f060016;
+        public static final int buttonSunday=0x7f06000b;
+        public static final int buttonThursday=0x7f06000f;
+        public static final int buttonTuesday=0x7f06000d;
+        public static final int buttonWednesday=0x7f06000e;
+        public static final int rel1=0x7f060003;
+        public static final int relativeLayout1=0x7f06000a;
+        public static final int seekBarVolume=0x7f060014;
+        public static final int spinnerSoundFile=0x7f060012;
+        public static final int textViewAlarmTitle=0x7f060000;
+        public static final int textViewBedTime1=0x7f060004;
+        public static final int textViewBedTime1AMPM=0x7f060005;
+        public static final int textViewBedTime2=0x7f060006;
+        public static final int textViewBedTime2AMPM=0x7f060007;
+        public static final int textViewBedTime3=0x7f060008;
+        public static final int textViewBedTime3AMPM=0x7f060009;
+        public static final int textViewSoundLabel=0x7f060013;
+        public static final int textViewSuggestion=0x7f060002;
+        public static final int textViewVolumeLabel=0x7f060015;
+        public static final int timePickerAlarm=0x7f060001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
+        public static final int app_name=0x7f050000;
     }
 }
--- src/edu/washington/ehoughl_krtyler1/AlarmClockActivity.java
+++ src/edu/washington/ehoughl_krtyler1/AlarmClockActivity.java
@@ -1,13 +1,85 @@
 package edu.washington.ehoughl_krtyler1;
 
+import java.util.Map;
 import android.app.Activity;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 public class AlarmClockActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) 
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        if(PreferencesExist())
+        {
+        	LoadAlarmFromPreferences(); 
+        }
+    }
+    
+    private void LoadAlarmFromPreferences()
+    {
+    	//get parameters from preferences
+    	SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+		Map<String, ?> m = prefs.getAll();
+    	
+		int alarmHour = (Integer) m.get((Object)"alarmHour");
+		int alarmMinute = (Integer) m.get((Object)"alarmHour");
+		String[] days = (String[]) m.get((Object)"days");
+		String soundFile = (String) m.get((Object)"soundFile");
+		Integer alarmVolume = (Integer) m.get((Object)"alarmVolume");
+		
+		//make UI reflect current alarm settings
+		TimePicker tp = (TimePicker)findViewById(R.id.timePickerAlarm);
+		tp.setCurrentHour(alarmHour);
+		tp.setCurrentMinute(alarmMinute);
+			
+    }
+    
+    public void onClickSunday(View view)
+	{
+    	Button b = (Button)view;
+    	b.getBackground().setColorFilter(0x66666600, android.graphics.PorterDuff.Mode.MULTIPLY);
+	}
+    
+    public void onClickSave(View view)
+	{
+    	SaveAlarmToPreferences();
+	} 
+    
+    private void SaveAlarmToPreferences()
+    {
+    	TimePicker tp = (TimePicker)findViewById(R.id.timePickerAlarm);	
+    	int alarmHour = tp.getCurrentHour();
+    	int alarmMinute = tp.getCurrentMinute();
+    	
+    	SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+    	Editor e = prefs.edit();
+		e.putBoolean("sleepwright", true);
+		e.putInt("alarmHour", alarmHour);
+		e.putInt("alarmMinute", alarmMinute);
+		e.commit();
+		
+		Toast toast = Toast.makeText(this, "Alarm Added!", Toast.LENGTH_SHORT);
+		toast.show();
+    }
+    
+    private boolean PreferencesExist()
+    {
+    	SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+		
+    	if (prefs.contains("sleepwright"))
+    	{
+    		return true;
+    	}
+    	
+    	return false;
     }
 }
