3734d2a79f4933a8c6149b5bfd3bef907a8e184c
Eric Houghland <eric.houghland@gmail.com>
2011-12-11 12:25:55 -0800

    Added save and cancel buttons to UI and removed Gradual volume increase and vibrate buttons

--- gen/edu/washington/ehoughl_krtyler1/R.java
+++ gen/edu/washington/ehoughl_krtyler1/R.java
@@ -14,21 +14,21 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
+        public static final int buttonCancel=0x7f050017;
         public static final int buttonFriday=0x7f050010;
         public static final int buttonMonday=0x7f05000c;
         public static final int buttonSaturday=0x7f050011;
+        public static final int buttonSave=0x7f050016;
         public static final int buttonSunday=0x7f05000b;
         public static final int buttonThursday=0x7f05000f;
         public static final int buttonTuesday=0x7f05000d;
         public static final int buttonWednesday=0x7f05000e;
-        public static final int checkBox1=0x7f050014;
-        public static final int checkBox2=0x7f050015;
         public static final int rel1=0x7f050003;
         public static final int relativeLayout1=0x7f05000a;
-        public static final int seekBar1=0x7f050013;
+        public static final int seekBar1=0x7f050014;
         public static final int spinner1=0x7f050012;
-        public static final int textView1=0x7f050016;
-        public static final int textView2=0x7f050017;
+        public static final int textView1=0x7f050013;
+        public static final int textView2=0x7f050015;
         public static final int textViewAlarmTitle=0x7f050000;
         public static final int textViewBedTime1=0x7f050004;
         public static final int textViewBedTime1AMPM=0x7f050005;
